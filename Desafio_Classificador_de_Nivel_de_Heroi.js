359e11f4c75a36b6593a3f763f506d5e01cfb485
Alejandro Alosilla <alejandroalosilla@gmail.com>
2024-02-27 13:27:10 -0300

    Create Desafio_Classificador_de_Nivel_de_Heroi.js

---
+++ Desafio_Classificador_de_Nivel_de_Heroi.js
@@ -0,0 +1,23 @@
+let nomeHeroi = "DarkNight"
+let expHeroi = 10001
+let nivel = ""
+
+if (expHeroi <= 1000){
+  nivel = "Ferro"
+} else if (expHeroi >= 1001 && expHeroi <= 2000 ){
+  nivel = "Bronze"
+} else if (expHeroi >= 2001 && expHeroi <= 5000 ){
+  nivel = "Prata"
+} else if (expHeroi >= 6001 && expHeroi <= 7000 ){
+  nivel = "Ouro"
+} else if (expHeroi >= 7001 && expHeroi <= 8000 ){
+  nivel = "Platina"
+} else if (expHeroi >= 8001 && expHeroi <= 9000 ){
+  nivel = "Ascendente"
+} else if (expHeroi >= 9001 && expHeroi <= 10000 ){
+  nivel = "Imortal"
+} else if (expHeroi >= 10001){
+  nivel = "Radiante"
+}
+
+console.log("O herói de nome " + nomeHeroi + " está no nível " + nivel + "!")
